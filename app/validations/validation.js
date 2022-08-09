351d9b0266dbdee753ec3a90dee60608767c1630
Victor <victorsmirnov@me.com>
2022-08-09 17:13:43 +0300

    add comments

--- app/validations/validation.js
+++ app/validations/validation.js
@@ -1,5 +1,6 @@
 import { body } from 'express-validator';
 
+// Auth validations
 export const signupValidation = [
   body('email', 'Email is wrong...').isEmail(),
   body('password', 'Password length min 5 characters').isLength({ min: 5 }),
@@ -11,7 +12,7 @@ export const loginValidation = [
   body('email', 'Email is wrong...').isEmail(),
   body('password', 'Password length min 5 characters').isLength({ min: 5 }),
 ];
-
+// Post validation
 export const postCreateValidation = [
   body('title', 'Enter article title...').isLength({ min: 3 }).isString(),
   body('text', 'Enter aticle text...').isLength({ min: 10 }).isString(),
